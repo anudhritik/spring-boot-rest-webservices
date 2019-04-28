cbba6e726c726b01824a7c6d3ca5cd290c7fedd6
anudhritik <anukreddy28@gmail.com>
2019-04-28 19:57:49 -0400

    Adding DELETE resource

--- src/main/java/com/demo/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +28,9 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retreiveUser(@PathVariable int id) {
 		User user = userDaoService.findOne(id);
-		if(user == null)
-			throw new UserNotFoundException("id-"+id);
-		
+		if (user == null)
+			throw new UserNotFoundException("id-" + id);
+
 		return user;
 	}
 
@@ -42,4 +43,13 @@ public class UserResource {
 
 		return ResponseEntity.created(location).build();
 	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUserbyId(@PathVariable int id) {
+		User user = userDaoService.deleteById(id);
+		if (user == null)
+			throw new UserNotFoundException("id-" + id);
+
+	}
+
 }
