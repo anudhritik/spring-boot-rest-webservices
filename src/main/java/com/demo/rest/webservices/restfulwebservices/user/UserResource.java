021d2164960522dd6fe0da97fde88589437896da
anudhritik <anukreddy28@gmail.com>
2019-04-28 18:41:37 -0400

    Adding User bean, DaoService, UserResource with get and post methods

---
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,41 @@
+package com.demo.rest.webservices.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+	@Autowired
+	private UserDaoService userDaoService;
+
+	// retrieve all users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return userDaoService.findAll();
+	}
+
+	// retrieveUser(int id)
+	@GetMapping("/users/{id}")
+	public User retreiveUser(@PathVariable int id) {
+		return userDaoService.findOne(id);
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+}
