b1819b0c4be021c87bdf7d506edad95619179a7f
anudhritik <anukreddy28@gmail.com>
2019-05-03 22:40:03 -0400

    Adding param and header versioning

--- src/main/java/com/demo/rest/webservices/restfulwebservices/versioning/PersonVersionController.java
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/versioning/PersonVersionController.java
@@ -14,4 +14,27 @@ public class PersonVersionController {
 	public PersonV2 getPersonV2() {
 		return new PersonV2(new Name("Anu", "Katanguri"));
 	}
+
+	// Request Param Versioning
+	@GetMapping(value = "person/param", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Anu");
+	}
+
+	@GetMapping(value = "person/param", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Anu", "Katanguri"));
+	}
+	
+	// Header Versioning
+	@GetMapping(value = "person/header", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Anu");
+	}
+
+	@GetMapping(value = "person/header", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Anu", "Katanguri"));
+	}
+
 }
