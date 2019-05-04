8fabbfd16617bbe1e316102c01857955b0ab356a
anudhritik <anukreddy28@gmail.com>
2019-05-03 23:31:12 -0400

    User entity and test date

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1, sysdate(), 'AA');
+insert into user values(2, sysdate(), 'BB');
+insert into user values(3, sysdate(), 'CC');
+insert into user values(4, sysdate(), 'DD');
